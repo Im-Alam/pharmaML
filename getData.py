3de3247ab2b8ae6f77f27b297c2259741153c53c
Im-Alam <imranalam04418@gmail.com>
2024-10-13 11:12:18 +0530

    demand api response formatted

---
+++ getData.py
@@ -0,0 +1,1 @@
+# To get data from database and import in Data folder
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 def load_supplier_scores(file_path: str) -> Dict[str, float]:
     try:
         with open(file_path, 'r') as file:
-            json_data = json.load(file)
+            return json.load(file)
         #formating of data
         
     except FileNotFoundError:
@@ -33,10 +33,20 @@ async def get_product_demand():
     try:
         with open('state/demand.json', 'r') as file:
             demand = json.load(file)
+        
+        with open('state/medicine_inventory.json', 'r') as file:
+            prod_names = json.load(file)
 
         if demand is None:
             raise HTTPException(status_code=404, detail="Product not found")
-        return demand
+        
+        transformed_data = {}
+        for product_id, demand_value in demand.items():
+            product_name = prod_names.get(product_id, "Unknown Product")
+            transformed_data[product_name] = {"demand": demand_value}
+
+
+        return transformed_data
     
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail="Demand file not found")
